3dc89616e588245721fd95aba50656d9cd2dfb47
Dian Nurita Hasanah <100190492+Diannurita@users.noreply.github.com>
2022-06-18 21:29:10 +0700

    Add files via upload

---
+++ CarRent.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class CarRent {
+    private Car car;
+    private CarRider rider;
+    private int rentDur;
+
+    public CarRent(Car car, CarRider rider, int rentDur){
+        this.car = car;
+        this.rider = rider;
+        this.rentDur = rentDur;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public int getRentDur() {
+        return rentDur;
+    }
+
+    public CarRider getRider() {
+        return rider;
+    }
+}
